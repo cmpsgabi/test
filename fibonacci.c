c73bd157a64d476a50c77ead24577e82ca11dbda
cmpsgabi <cmpsgabi@gmail.com>
2025-01-21 02:13:42 -0300

    fibonacci code

--- fibonacci.c
+++ fibonacci.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int fibonacci(int n);
+
+int main(){
+    int number;
+
+    scanf("%d", &number);
+
+    printf("%d", fibonacci(number));
+
+    return 0;
+}
+
+int fibonacci(int n){
+    if(n == 0){
+        return 0;
+    }
+
+    if(n == 1){
+        return 1;
+    }
+
+    return fibonacci(n-1) + fibonacci(n-2);
+}
